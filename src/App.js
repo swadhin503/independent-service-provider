5ec0468d3128a15de210727e8c36ca47e027aef0
swadhin503 <tawhiddwinswadhin@gmail.com>
2022-04-18 23:23:42 +0600

    404 page added

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Login from './components/Login/Login';
 import Signup from './components/Signup/Signup';
 import Checkout from './components/Checkout/Checkout';
 import RequireAuth from './components/RequireAuth/RequireAuth';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
         <Route path="/about" element={<About></About>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/signup" element={<Signup></Signup>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
         <Route path="/checkout" element={
           <RequireAuth>
             <Checkout></Checkout>
